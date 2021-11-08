709da35a241fa12118fb3d829bc18f6b9ad68942
albadauto <joseadauto923@gmail.com>
2021-11-08 12:02:19 -0300

    Esqueci a senha

--- routes/Recover.js
+++ routes/Recover.js
@@ -21,15 +21,19 @@ router.get("/", (req, res) => {
 router.post("/verify", async (req, res) => {
   try {
     const result = await LoginModel.find({ email: req.body.email });
-
     if (result.length > 0) {
+      var idPass = "";
+      result.forEach((resultado) => {
+        idPass = resultado._id;
+      });
       await transporter.sendMail({
         from: process.env.MAIL_USER,
         to: req.body.email,
         subject: "TESTE",
-        text: "Teste",
+        text: process.env.SITE + "recover"+"/updatepass/"+idPass,
       });
       req.flash("success", "Email");
+      req.session.idPass = idPass;
       res.redirect("/recover");
     } else {
       req.flash("error", "Email inexistente!");
@@ -39,5 +43,32 @@ router.post("/verify", async (req, res) => {
     console.log(err);
   }
 });
+
+
+router.get("/updatepass/:id", (req, res) => {
+  try{
+    if (typeof req.session.idPass !== "undefined") {
+      req.params.id = req.session.idPass
+      res.render("UpdatePass")
+    }else{
+      res.redirect('/recover')
+    }
+  }catch(err){
+    console.log(err)
+  }
+  
+});
+
+router.post("/atualizar", async (req, res) => {
+  try{
+    const update = {
+      senha: req.body.updatePass
+    }
+    await LoginModel.findOneAndUpdate({_id: req.session.idPass}, update).clone()
+    res.send("Atualizado!")
+  }catch(err){
+    console.log(err)
+  }
+})
 
 module.exports = router;
